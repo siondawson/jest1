7fc3ecc970bd72b1f4ee27ef1615505b47b02e51
siondawson <>
2023-05-22 12:31:41 +0000

    got to addgame function video

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
- const { game } = require("../game");
+ const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require("../game");
 
 
  beforeAll(() => {
@@ -21,12 +21,62 @@
          expect("currentGame" in game).toBe(true);
      });
      test("playerMoves key exists", () => {
-        expect("playerMoves" in game).toBe(true);
+         expect("playerMoves" in game).toBe(true);
+     });
+     test("choices key exists", () => {
+         expect("choices" in game).toBe(true);
+     });
+     test("choices contain correct ids", () => {
+         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+     });
+ });
+ 
+ describe("newGame works correctly", () => {
+     beforeAll(() => {
+         game.score = 42;
+         game.playerMoves = ["button1", "button2"];
+         game.currentGame = ["button1", "button2"];
+         document.getElementById("score").innerText = "42";
+         newGame();
+     });
+     test("should set game score to zero", () => {
+         expect(game.score).toEqual(0);
+     });
+     test("should display 0 for the element with id of score", () => {
+         expect(document.getElementById("score").innerText).toEqual(0);
+     });
+     test("should clear the player moves array", () => {
+         expect(game.playerMoves.length).toBe(0);
+     });
+     test("should be one move in the computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
+     });
+ });
+
+ describe("gameplay works correctly", () => {
+    beforeEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+        addTurn();
     });
-    test("choices key exists", () => {
-        expect("choices" in game).toBe(true);
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
     });
-    test("choices contain correct ids", () => {
-        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    test("addTurn add new turn to the game", () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
     });
+    test("should add correct class to light up the button", () => {
+        let button = document.getElementById(game.currentGame[0]);
+        lightsOn(game.currentGame[0]);
+        expect(button.classList).toContain("light");
+    });
+    test("showTurns should update game.turnNumber", () => {
+        game.turnNumber = 42;
+        showTurns();
+        expect(game.turnNumber).toBe(0);
+    })
  });
